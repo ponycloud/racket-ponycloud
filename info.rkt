9f689602dd57df5f90091415db4448548beff18b
Jan Dvořák <mordae@anilinux.org>
2013-11-27 08:39:37 +0100

    include planet package deps
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
 (define deps '("sysfs" "rtnl" "udev" "libvirt" "zmq" "tasks"
-               "xexpr-path" "planet-williams-uuid1"))
+               "xexpr-path" "planet-williams-uuid1" "planet-soegaard-digest1"))
 
 ; vim:set ts=2 sw=2 et:

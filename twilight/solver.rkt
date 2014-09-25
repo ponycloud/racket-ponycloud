c809c6c3091b4d398169100fb56c574e67f477fd
Jan Dvořák <mordae@anilinux.org>
2014-09-25 16:53:41 +0200

    added reference-counting dependency solver
    Our rc-solver wraps the solver from dds package and adds de-duplication
    and reference counting, allowing our managers to define identical
    targets multiple times.
    
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

---
+++ twilight/solver.rkt
@@ -0,0 +1,64 @@
+#lang racket/base
+;
+; Reference-Counting Dependency Solver
+;
+; The actual dependency-solving and target handling is taken care of
+; by dds solver, but we add the reference counting in order to simplify
+; our network / storage / instance managers.
+;
+
+(require racket/contract
+         racket/match)
+
+(require misc1/syntax
+         dds)
+
+(provide
+  (contract-out
+    (rc-solver? predicate/c)
+    (make-rc-solver (-> solver? rc-solver?))
+    (rc-solver-create! (-> rc-solver? target? void?))
+    (rc-solver-destroy! (-> rc-solver? target? void?))))
+
+
+(struct rc-solver
+  (real targets)
+  #:property prop:evt (struct-field-index real))
+
+(struct rc-target
+  (real (refcnt #:mutable)))
+
+
+(define (make-rc-solver real)
+  (rc-solver real (make-hash)))
+
+
+(define (incref! target)
+  (let ((refcnt (rc-target-refcnt target)))
+    (set-rc-target-refcnt! target (add1 refcnt))
+    (= 0 refcnt)))
+
+(define (decref! target)
+  (let ((refcnt (rc-target-refcnt target)))
+    (set-rc-target-refcnt! target (sub1 refcnt))
+    (= 1 refcnt)))
+
+
+(define (rc-solver-create! solver real-target)
+  (define (make-rc-target)
+    (rc-target real-target 0))
+
+  (match-let (((rc-solver real-solver targets) solver))
+    (let ((target (hash-ref! targets real-target make-rc-target)))
+      (when (incref! target)
+        (solver-create! real-solver real-target)))))
+
+
+(define (rc-solver-destroy! solver real-target)
+  (match-let (((rc-solver real-solver targets) solver))
+    (when* ((target (hash-ref targets real-target #f)))
+      (when (decref! target)
+        (solver-destroy! real-solver real-target)))))
+
+
+; vim:set ts=2 sw=2 et:

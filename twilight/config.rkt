e9af7154a9cabf1c818faed040046646c016a4ac
Jan Dvořák <mordae@anilinux.org>
2014-11-05 09:26:17 +0100

    improve interface of configuration objects
    Add a generic method to detect when the object is going to be
    destroyed and should be deleted from the configuration when it's
    task finishes.
    
    Also change how units are spawned. Instead of receiving a manager,
    pass them a hash table with other units so that a snapshot can be
    passed to them with every transaction.
    
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- twilight/config.rkt
+++ twilight/config.rkt
@@ -13,17 +13,18 @@
   (contract-out
     (config-type (-> config? symbol?))
     (config-pkey (-> config? any/c))
-    (config-unit (-> config? manager/c unit?))))
+    (config-delete? (-> config? boolean?))
+    (config-spawn-unit (-> config? units/c unit?))))
 
-(define manager/c
-  (object/c
-    (get-unit (->m symbol? any/c unit?))))
+(define units/c
+  (hash/c (list/c symbol? any/c) unit?))
 
 
 (define-generics config
   (config-type config)
   (config-pkey config)
-  (config-unit config manager))
+  (config-delete? config)
+  (config-spawn-unit config other-units))
 
 
 ; vim:set ts=2 sw=2 et:
